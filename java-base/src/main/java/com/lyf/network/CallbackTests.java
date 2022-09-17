64b5b9606d2fbc73049ec0634508086d9776e8a2
lyf <674619459@qq.com>
2022-09-17 22:59:59 +0800

    [practice]fix the timeout bug

--- java-base/src/main/java/com/lyf/network/CallbackTests.java
+++ java-base/src/main/java/com/lyf/network/CallbackTests.java
@@ -13,12 +13,12 @@ public class CallbackTests {
         // 模拟通信
         long cur = System.currentTimeMillis();
         long mockCommunicateTs = 1000 + new Random().nextLong();
-        
         while (System.currentTimeMillis()-cur<mockCommunicateTs){
             // 通信中，且在允许超时范围内
             if(System.currentTimeMillis()-cur >= time){
                 event.setMsg("超时");
                 callBack.onError(event);
+                return;
             }
         }
         // 返回的结果处理
@@ -37,6 +37,6 @@ public class CallbackTests {
             public void onError(Event event) {
                 System.out.println("failed");
             }
-        },3L);
+        },1L);
     }
 }
