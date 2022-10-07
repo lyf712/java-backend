6d5f65138277ce86c46529831683cedea9bb1291
lyf <674619459@qq.com>
2022-10-07 20:16:45 +0800

    [practice]LC practice(thread problems-lc:1195)

---
+++ java-base/src/main/java/com/lyf/alg/leetcode/LC1195.java
@@ -0,0 +1,26 @@
+package com.lyf.alg.leetcode;
+
+import java.util.function.IntConsumer;
+
+/**
+ * 1195. 交替打印字符串
+ * https://leetcode.cn/problems/fizz-buzz-multithreaded/
+ * 采用lock进行抢锁控制，并判断当前i
+ * @author liyunfei
+ */
+public class LC1195 {
+    
+    public static void main(String[] args) {
+        IntConsumer consumer = new IntConsumer() {
+            @Override
+            public void accept(int value) {
+        
+            }
+        };
+        //consumer.accept();
+        consumer.accept(1);
+        //consumer.
+        int[]nums = new int[1];
+//        nums.length;
+    }
+}
