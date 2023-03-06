c57170732aca3432852394b9eaa19d6b85a044d3
lyf <674619459@qq.com>
2023-03-06 20:44:10 +0800

    [practice]daily lc dfs split problem

--- java-base/src/main/java/com/lyf/alg/leetcode/graph/DFSProblem.java
+++ java-base/src/main/java/com/lyf/alg/leetcode/graph/DFSProblem.java
@@ -16,10 +16,7 @@
 
 package com.lyf.alg.leetcode.graph;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author liyunfei
@@ -89,6 +86,75 @@ public class DFSProblem {
 
 
 
+
+    /**
+     * 回溯算法：dfs深度搜索，符号则，加入，不符合进行dfs
+     */
+    List<List<String>> rs = new ArrayList<>();
+    LinkedList<String> path = new LinkedList<>();
+    public String[][] partition(String s) {
+        dfs(0,s);
+        String[][]rs0 = new String[rs.size()][];
+        for(int i=0;i<rs.size();i++){
+            rs0[i] = new String[rs.get(i).size()];
+            for(int j=0;j<rs.get(i).size();j++){
+                rs0[i][j]=rs.get(i).get(j);
+            }
+        }
+        return rs0;//rs0;
+    }
+    void dfs(int index,String s){
+        if(index>=s.length()){
+            rs.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i=index;i<s.length();i++){
+            // 选择从哪里切
+            if(!validate(s.substring(index,i+1))){
+                //path.addLast()
+                continue;
+            }
+            path.addLast(s.substring(index,i+1));
+            dfs(i+1,s);
+            path.removeLast();
+        }
+    }
+
+    boolean validate(String s){
+        int left = 0,right = s.length()-1;
+        while(left<=right){
+            if(s.charAt(left++)!=s.charAt(right--)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 二分图
+    int[]record;// -1,0,1标记染色
+    boolean valid=true;
+    public boolean isBipartite(int[][] graph) {
+        // DFS + 染色处理
+        record = new int[graph.length];
+        for(int i=0;i<graph.length && valid;i++){
+            if(record[i]==0){// 未标记
+                record[i] = 1;
+                dfs(i,graph);
+            }
+        }
+        return valid;
+    }
+    void dfs(int indexNo,int[][]graph){
+        for(int neibor:graph[indexNo]){
+            if(record[neibor]==0){// 临边未染色则继续dfs
+                record[neibor] = (-1)*record[indexNo];
+                dfs(neibor,graph);
+            }else if(record[neibor]==record[indexNo]){//染色则判断是否冲突
+                valid=false;
+                return ;
+            }
+        }
+    }
 
 
 }
